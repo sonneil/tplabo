0f0a4eec5a62e61be921837624c9ff61cff0c976
Viole <violeroizman@gmail.com>
2015-04-20 00:19:10 -0300

    contructor basico

--- correpocoyo.h
+++ correpocoyo.h
@@ -14,134 +14,53 @@ class CorrePocoyo{
 
   public:
 
-	/*
-	 * Crea una nueva carrera
-	 */
 	CorrePocoyo();
 
-	/*
-	 * Una vez copiada, ambas CorrePocoyos deben ser independientes, 
-	 * es decir, cuando se borre una no debe borrar la otra.
-	 */	
 	CorrePocoyo(const CorrePocoyo<T>&);
-	
-	/*
-	 * Acordarse de liberar toda la memoria!
-	 */	 
+
 	~CorrePocoyo();
 
-	/*
-	 * Agrega un nuevo corredor al CorrePocoyo. Lo agregará al final 
-	 */
 	void nuevoCorredor(const T&);
-	
-	/*
-	 * Agrega un nuevo corredor al CorrePocoyo. El primer T es el corredor a agregar y lo hace delante del segundo
-	 *
-	 * PRE: Existe el segundo corredor 
-	 */
+
 	void nuevoCorredor(const T&, const T&);
-	
-	/*
-	 * Elimina del pocoyo al que se envía
-	 *
-	 * PRE: Existe el corredor
-	 */
+
 	void seCansa(const T&);
-	/*
-	 * El corredor pasa al que está adelante 
-	 *
-	 * PRE: Existe un corredor delante de él y T es un corredor 
-	 */
+
 	void sobrepasar(const T&);
-	
-	/*
-	 * Devuelve el corredor que está siendo filmado.
-	 *
-	 * PRE: Hay adeptos en la CorrePocoyo.
-	 */
+
 	const T& corredorFilmado() const;
-	
-    /*
-	 * Devuelve el próximo elemento según el orden dado.
-	 * Pasa a filmar al corredor de atás 
-	 *
-	 * PRE: Hay corredore en la CorrePocoyo.
-	 */
+
 	void filmarProxPerdedor();
-	    /*
-	 * Devuelve el próximo elemento según el orden dado.
-	 * Pasa a filmar al corredor de adelante 
-	 *
-	 * PRE: Hay corredore en la CorrePocoyo.
-	 */
+
 	void filmarProxExitoso();
-	
-	/*
-	 * Devuelve al Primero. 
-	 *
-	 * PRE: Hay elementos 
-	 */
+
 	const T& damePrimero() const;	
 
-	/*
-	 * Devuelve la posición del elemento pasado por parámetro. 
-	 *
-	 * PRE: Existe ese corredor 
-	 */
 	int damePosicion(const T& ) const;	
 
-	/*
-	 * Devuelve el corredor de la posición. 
-	 *
-	 * PRE: Existe al menos esa cantidad de corredores en la carrera
-	 */
 	const T& dameCorredorEnPos(int) const;	
 
-
-
-
-	/*
-	 * Dice si la CorrePocoyo tiene o no corredores.
-	 */
 	bool esVacia() const;
 
-	
-	/*
-	 * Devuelve la cantidad de corredores de la CorrePocoyo.
-	 */
 	int tamanio() const;	
 
-	/*
-	 * Devuelve true si las CorrePocoyos son iguales.
-	 */
 	bool operator==(const CorrePocoyo<T>&) const;	
-	
-	/*
-	 * Debe mostrar la carrera por el ostream (y retornar el mismo).
-	 * CorrePocoyo vacio: []
-	 */
+
 	ostream& mostrarCorrePocoyo(ostream&) const;
-	
-	
+
   private:
-	/*
-	 * No se puede modificar esta funcion.
-	 */
+
 	CorrePocoyo<T>& operator=(const CorrePocoyo<T>& otra) {
 		assert(false);
 		return *this;
 	}
 
-	/*
-	 * Aca va la implementación del nodo.
-	 */
 	struct Nodo {
 		Nodo* nextCorredor;
 		T currCorredor;
 	};
 	
-	int tamanio;
+	int tamanioc;
 	Nodo* prmCorredor;
 	Nodo* filmando;
 };
@@ -151,12 +70,38 @@ ostream& operator<<(ostream& out, const CorrePocoyo<T>& a) {
 	return a.mostrarCorrePocoyo(out);
 }
 
-CorrePocoyo CorrePocoyo::CorrePocoyo(){
-	this.tamanio=0;
+template<class T>
+CorrePocoyo<T>::CorrePocoyo(){
+	this.tamanioc=0;
 	this.prmCorredor=NULL;
 	this.filmando=NULL;
 }
 
+template<class T>
+void CorrePocoyo<T>::nuevoCorredor(const T& nuevo){
+	Nodo * nuevoNodo = new Nodo;
+	nuevoNodo->nextCorredor=this.prmCorredor;
+	nuevoNodo->currCorredor=new T(nuevo);
+	if(this.tamanioc==0){
+		this.filmando=nuevoNodo;
+	}else{
+		Nodo * actual=this.prmCorredor;
+		while((actual->nextCorredor->currCorredor)!=(this->prmCorredor->currCorredor)){
+			actual=(actual->nextCorredor);
+		}
+		actual->nextCorredor=nuevoNodo;
+	}
+	tamanioc++;
+}
 
+template<class T>
+int CorrePocoyo<T>::tamanio() const{
+	return this.tamanioc; 
+}
+
+template<class T>
+bool CorrePocoyo<T>::esVacia() const{
+	return this.tamanioc==0; 
+}
 
 #endif //CORREPOCOYO_H_
